e21fdc50c7a5f81127cc85ed07044027dfccd815
Kustikov <pasha_k1987@mail.ru>
2020-05-12 18:12:39 +0300

    12_05_2020 добавление reverse arr

--- reverse1.js
+++ reverse1.js
@@ -1,1 +1,13 @@
-reverse правильный метод без библиотек
+const reverse = (coll) => {
+	const length = coll.length;
+	for (let i = 0; i < length / 2; i += 1) {
+		const mirrorIndex = (length - 1) - i;
+		const temp = coll[i];
+		coll[i] = coll[mirrorIndex];
+		coll[mirrorIndex] = temp;
+	}
+	console.log(coll);
+	return coll;
+}
+
+reverse([0, 5, 7, 8])
